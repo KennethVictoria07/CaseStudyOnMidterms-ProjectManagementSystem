e5c8436e3a008b09205bf331c256287c06607def
SenpaiAzzie <tudocdaleasher70@gmail.com>
2025-04-06 17:14:31 +0800

    added context folder that contains AuthContext.jsx & ProjectContext.jsx

---
+++ frontend/context/AuthContext.jsx
@@ -0,0 +1,192 @@
+import React, { useReducer, useContext, createContext } from "react";
+import axios from "axios";
+
+const context = createContext();
+
+export default function AUTH({ children }) {
+  const INIT_STATE = {
+    loading: false,
+    error: "",
+    status: 0,
+    userData: {},
+    userToken: "",
+    logInUser: {
+      email: "",
+      password: "",
+    },
+  };
+
+  function REDUCER(state, action) {
+    switch (action.type) {
+      case "START_LOADING":
+        return {
+          ...state,
+          loading: true,
+        };
+      case "FINISH_LOADING":
+        return {
+          ...state,
+          loading: false,
+        };
+
+      case "SET_USER":
+        return {
+          ...state,
+          userData: action.payload.user,
+          userToken: action.payload.token,
+        };
+
+      case "CLEAR_USER":
+        return {
+          ...state,
+          userData: {},
+          userToken: "",
+          logInUser: {
+            email: "",
+            password: "",
+          },
+        };
+
+      case "NOT_AUTHORIZED":
+        return {
+          ...state,
+          user_data: {},
+          token: "",
+          status: 401,
+        };
+
+      case "ENTERING_EMAIL":
+        return {
+          ...state,
+          logInUser: {
+            ...state.logInUser,
+            email: action.payload.email,
+          },
+        };
+
+      case "ENTERING_PASSWORD":
+        return {
+          ...state,
+          logInUser: {
+            ...state.logInUser,
+            password: action.payload.password,
+          },
+        };
+
+      case "SUCCESS_FETCH":
+        return {
+          ...state,
+          error: "",
+          status: action.payload.status,
+        };
+
+      case "FAIL_FETCH":
+        return {
+          ...state,
+          error: action.payload.error,
+          status: action.payload.status,
+        };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(REDUCER, INIT_STATE);
+
+  const login = async () => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      console.log(state.logInUser.email, state.logInUser.password);
+      const login = await axios.post("http://127.0.0.1:8000/api/login/", {
+        email: state.logInUser.email,
+        password: state.logInUser.password,
+      });
+      if (login.status === 200) {
+        console.log(login.data);
+
+        dispatch({
+          type: "SET_USER",
+          payload: { user: login.data.user, token: login.data.token },
+        });
+        // due to asynchronous nature of states, we must store data on session storage for the program to run after reloading
+        sessionStorage.setItem("token", login.data.token);
+        sessionStorage.setItem("email", login.data.user.email);
+        sessionStorage.setItem("name", login.data.user.name);
+        sessionStorage.setItem("role", login.data.user.role);
+        sessionStorage.setItem("id", login.data.user.id);
+        return true;
+      }
+      dispatch({ type: "NOT_AUTHORIZED" });
+      return false;
+    } catch (error) {
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  const logout = async () => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const login = await axios.post(
+        "http://127.0.0.1:8000/api/logout/",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (login.status === 200) {
+        dispatch({
+          type: "CLEAR_USER",
+        });
+        sessionStorage.removeItem("token");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("role");
+        sessionStorage.removeItem("id");
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  return (
+    <context.Provider
+      value={{
+        loading: state.loading,
+        error: state.error,
+        status: state.status,
+        userData: state.userData,
+        userToken: state.userToken,
+        status: state.status,
+        dispatch,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </context.Provider>
+  );
+}
+
+export function CONSUMEAUTH() {
+  return useContext(context);
+}
---
+++ frontend/context/ProjectContext.jsx
@@ -0,0 +1,382 @@
+import React, { useReducer, useContext, createContext } from "react";
+import axios from "axios";
+import { CONSUMEAUTH } from "./AuthContext";
+
+const context = createContext();
+
+export default function PROJ({ children }) {
+  const AUTH = CONSUMEAUTH();
+  const INIT_STATE = {
+    loading: false,
+    error: "",
+    status: 0,
+    project: {},
+    task: {},
+    projectList: [],
+    taskList: [],
+    addEditProjectForm: {
+      title: "",
+      description: "",
+    },
+    addTask: {
+      title: "",
+      description: "",
+    },
+  };
+
+  function REDUCER(state, action) {
+    switch (action.type) {
+      case "START_LOADING":
+        return {
+          ...state,
+          loading: true,
+        };
+      case "FINISH_LOADING":
+        return {
+          ...state,
+          loading: false,
+        };
+
+      case "ENTERING_TITLE":
+        return {
+          ...state,
+          addEditProjectForm: {
+            ...state.addEditProjectForm,
+            title: action.payload.title,
+          },
+        };
+
+      case "ENTERING_DESCRIPTION":
+        return {
+          ...state,
+          addEditProjectForm: {
+            ...state.addEditProjectForm,
+            description: action.payload.description,
+          },
+        };
+
+      case "ENTERING_TASK_TITLE":
+        return {
+          ...state,
+          addTask: { ...state.addTask, title: action.payload.title },
+        };
+
+      case "ENTERING_TASK_DESCRIPTION":
+        return {
+          ...state,
+          addTask: {
+            ...state.addTask,
+            description: action.payload.description,
+          },
+        };
+
+      case "PROJECTLIST":
+        return {
+          ...state,
+          projectList: action.payload.projectList,
+        };
+
+      case "TASKLIST":
+        return {
+          ...state,
+          taskList: action.payload.taskList,
+        };
+
+      case "TASK":
+        return {
+          ...state,
+          task: action.payload.task,
+        };
+
+      case "PROJECT":
+        return {
+          ...state,
+          project: action.payload.project,
+        };
+
+      case "SUCCESS_FETCH":
+        return {
+          ...state,
+          error: "",
+          status: action.payload.status,
+        };
+
+      case "FAIL_FETCH":
+        return {
+          ...state,
+          error: action.payload.error,
+          status: action.payload.status,
+        };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(REDUCER, INIT_STATE);
+
+  const getProjects = async () => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const get = await axios.get("http://127.0.0.1:8000/api/projects/", {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      if (get.status === 200) {
+        dispatch({
+          type: "PROJECTLIST",
+          payload: { projectList: get.data.projects },
+        });
+
+        dispatch({
+          type: "PROJECT",
+          payload: { project: get.data.projects[0] },
+        });
+
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      console.log(error);
+
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  const addProject = async (user_id) => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const add = await axios.post(
+        "http://127.0.0.1:8000/api/projects/",
+        {
+          title: state.addEditProjectForm.title,
+          description: state.addEditProjectForm.description,
+          status: "in_progress",
+          user_id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (add.status === 201) {
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      console.log(error);
+
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  const updateStatus = async (id, status, title, description) => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const edit = await axios.put(
+        `http://127.0.0.1:8000/api/projects/${id}`,
+        {
+          status,
+          title,
+          description,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (edit.status === 200) {
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      console.log(error);
+
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  const deleteProject = async (id) => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const remove = await axios.delete(
+        `http://127.0.0.1:8000/api/projects/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (remove.status === 204) {
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      console.log(error);
+
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  const getTasks = async (id) => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const get = await axios.get(
+        `http://127.0.0.1:8000/api/projects/${id}/tasks`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (get.status === 200) {
+        console.log(get.data);
+
+        dispatch({
+          type: "TASKLIST",
+          payload: { taskList: get.data },
+        });
+
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      console.log(error);
+
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  const addProjectTask = async (project_id) => {
+    dispatch({ type: "START_LOADING" });
+    try {
+      const add = await axios.post(
+        `http://127.0.0.1:8000/api/projects/${project_id}/tasks`,
+        {
+          title: state.addTask.title,
+          description: state.addTask.description,
+          status: "in_progress",
+          project_id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (add.status === 201) {
+        return true;
+      }
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: "Unauthorized", status: 401 },
+      });
+      return false;
+    } catch (error) {
+      console.log(error);
+
+      dispatch({
+        type: "FAIL_FETCH",
+        payload: { error: error.message, status: 500 },
+      });
+      return false;
+    } finally {
+      dispatch({ type: "FINISH_LOADING" });
+    }
+  };
+
+  // projects/{project}/tasks
+  //   {
+  //     loading: false,
+  //     error: "",
+  //     status: 0,
+  //     project: {},
+  //     task: {},
+  //     projectList: [],
+  //     taskList: [],
+  //     addEditProjectForm: {
+  //       title: "",
+  //       description: "",
+  //     },
+  //     addTask: "",
+  //   };
+
+  return (
+    <context.Provider
+      value={{
+        loading: state.loading,
+        error: state.error,
+        status: state.status,
+        project: state.project,
+        task: state.task,
+        projectList: state.projectList,
+        taskList: state.taskList,
+        addEditProjectForm: state.addEditProjectForm,
+        addTask: state.addTask,
+        dispatch,
+        getProjects,
+        addProject,
+        updateStatus,
+        deleteProject,
+        getTasks,
+        addProjectTask,
+      }}
+    >
+      {children}
+    </context.Provider>
+  );
+}
+
+export function CONSUMEPROJ() {
+  return useContext(context);
+}
