73d894f9c5dcbefd69bf9a382f7e3208059fc740
Joshua000842 <saladagajoshua94@gmail.com>
2025-04-07 02:36:49 +0800

    pushed modified api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProjectController;
 use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/register', [RegisterController::class, 'register']);
 Route::middleware(['auth:api'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     // Route::middleware('role:admin')->group(function () {
